8232ff6f749062088a7b4d828bc6e1b1240055cb
Kim Borgen <kim@kimborgen.com>
2023-11-03 15:05:34 +0100

    test position update rate #1

--- minedojo_validate.py
+++ minedojo_validate.py
@@ -3,10 +3,14 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy as np
-env = minedojo.make(task_id="harvest_milk", image_size=(160, 256))
+from config.config import load_config
 
-print("task prompt: ", env.task_prompt)
-print("task guidance: ", env.task_guidance)
+cfg = load_config("config/default_config.yaml")
+#env = minedojo.make(task_id="harvest_milk", image_size=(160, 256))
+env = minedojo.make(task_id=cfg.minedojo.task_id, image_size=tuple(cfg.minedojo.minecraft_rgb_shape), world_seed=cfg.minedojo.world_seed, generate_world_type=cfg.minedojo.generate_world_type, specified_biome=cfg.minedojo.specified_biome)
+
+#print("task prompt: ", env.task_prompt)
+#print("task guidance: ", env.task_guidance)
 fig, ax = plt.subplots()
 canvas = fig.canvas  # Get the canvas
 plt.show(block=False)  # Add this line to display the window
@@ -14,6 +18,9 @@ obs = env.reset()
 img = ax.imshow(np.random.randint(0, 256, (160, 256, 3), dtype=np.uint8))
 for i in range(100):
     obs_space = env.observation_space
+    pos = obs["location_stats"]["pos"][:3]
+    print(pos)
+
     #print("position", obs_space["location_stats"]["pos"])
     #print("img ", obs["rgb"])
 
@@ -25,6 +32,7 @@ for i in range(100):
 
     action[0] = 1
     action[2] = 1
+    action[4] = 13
 
     next_obs, reward, done, info = env.step(action)
     obs = next_obs
